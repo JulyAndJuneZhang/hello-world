bf44971d242f77914db00e7c7b062fe52ee4e77d
zhangkaiqi <15234092792@163.com>
2019-02-15 17:15:46 +0800

    Initial commit

---
+++ src/learn/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package learn.demo;
+
+/**
+ * zkq19100 2019/2/15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+            }
